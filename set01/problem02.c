3a144431e97b32670ae5684279c0a20de1876736
tvchblur <141307481+tvchblur@users.noreply.github.com>
2023-10-12 12:11:02 +0530

    committed 10/12/2023, 12:11:02 PM

--- set01/problem02.c
+++ set01/problem02.c
@@ -1,2 +1,12 @@
 //2. Write a C program to add two numbers.
+#include <stdio.h>
+
+int main(void){
+    int a, b;
+    printf("Enter two numbers to be added:\n");
+    scanf("%d\n%d", &a, &b);
+    int sum=a+b;
+    printf("The sum of %d and %d is %d.\n", a, b, sum);
+    return 0;
+}
 
