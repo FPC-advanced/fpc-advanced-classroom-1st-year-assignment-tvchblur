74608da49fd0ff26f6ca59952df11142b09c2091
tvchblur <141307481+tvchblur@users.noreply.github.com>
2023-10-16 23:00:10 +0530

    committed 10/16/2023, 11:00:10 PM

--- set01/problem05.c
+++ set01/problem05.c
@@ -1,1 +1,49 @@
+/*
+5. Write a C program to compare three numbers using **pass by value**
 
+***Function Declarations***
+
+```c
+int input();
+int compare(int a, int b, int c);
+void output(int a, int b, int c, int largest);
+```
+*/
+
+#include <stdio.h>
+
+int input(){
+    int n;
+    printf("Enter a number to be compared:\n");
+    scanf("%d", &n);
+    return n;
+}
+
+int compare(int a, int b, int c){
+    int largest;
+    if(a>=b&&a>=c){
+        largest = a;
+    }
+    else if(b>=c){
+        largest = b;
+    }
+    else{
+        largest = c;
+    }
+
+    return largest;
+}
+
+void output(int a, int b, int c, int largest){
+    printf("The largest of %d, %d and %d is %d.\n", a, b, c, largest);
+}
+
+int main(void){
+    int a, b, c, largest;
+    a = input();
+    b = input();
+    c = input();
+    largest = compare(a, b, c);
+    output(a,b,c,largest);
+    return 0;
+}
