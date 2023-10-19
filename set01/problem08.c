0387a6996ea98fb009f6d4b22529a59393756f14
tvchblur <141307481+tvchblur@users.noreply.github.com>
2023-10-19 16:07:27 +0530

    committed 10/19/2023, 4:07:26 PM

---
+++ set01/problem08.c
@@ -0,0 +1,24 @@
+/*
+8. Write a C program to find sum of _n_ different numbers entered by the user.
+
+***Function Declarations***
+
+```c
+int input_array_size();
+void input_array(int n, int a[n]);
+int sum_n_array(int n, int a[n]);
+void output(int n, int a[n],int sum);
+```
+
+***Input***
+```
+Input array size: 3
+Input the array:
+1 7 11
+```
+
+***Output***
+```
+1+7+11 is 19
+```
+*/
