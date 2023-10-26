d46d634144ae8cf5f19c8c9888571644326a098a
tvchblur <141307481+tvchblur@users.noreply.github.com>
2023-10-26 16:08:30 +0530

    committed 10/26/2023, 4:08:28 PM

--- set01/problem08.c
+++ set01/problem08.c
@@ -22,3 +22,46 @@ Input the array:
 1+7+11 is 19
 ```
 */
+
+#include <stdio.h>
+
+int input_array_size(){
+    int n;
+    printf("Input the array size:\n");
+    scanf("%d", &n);
+    return n;
+}
+
+void input_array(int n, int a[n]){
+    printf("Input the array:\n");
+    int index=0;
+    for(index=0; index<n; index++){
+        scanf("%d", &a[index]);
+    }
+}
+
+int sum_n_array(int n, int a[n]){
+    int sum=0;
+    int index=0;
+    for(index=0;index<n;index++){
+        sum = sum + a[index];
+    }
+
+    return sum;
+}
+
+void output(int n, int a[n],int sum){
+    printf("The sum of the elements of the array is %d.", sum);
+
+}
+
+int main(void){
+    int n;
+    n = input_array_size();
+    int a[n];
+    input_array(n, a);
+    int sum =0;
+    sum = sum_n_array(n, a);
+    output(n, a, sum);
+
+}
