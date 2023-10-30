84a7be18ca54fcfb9ea56d5417cb0e47f8c033c4
tvchblur <141307481+tvchblur@users.noreply.github.com>
2023-10-30 09:24:21 +0530

    committed 10/30/2023, 9:24:20 AM

--- set01/problem10.c
+++ set01/problem10.c
@@ -1,3 +1,19 @@
 /*
+9. Write a C program to find the [square root](https://en.wikipedia.org/wiki/Methods_of_computing_square_roots#Babylonian_method) of a number.
 
+***Function Declarations***
+```c
+float input();
+float square_root(float n);
+void output(float n, float sqrroot);
+```
+
+***Input***
+```
+Enter number: 49
+```
+
+***Output***
+```
+Square root of 49.0 is 7.0
 */
