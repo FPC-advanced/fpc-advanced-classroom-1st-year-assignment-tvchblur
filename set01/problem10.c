efeed2721747ce1b7a0e09603a6e8505e5413f6a
tvchblur <141307481+tvchblur@users.noreply.github.com>
2023-10-31 11:41:02 +0530

    committed 10/31/2023, 11:41:01 AM

--- set01/problem10.c
+++ set01/problem10.c
@@ -22,26 +22,44 @@ Enter the second string: world*/
 
 void input_two_strings(char *string1, char *string2){
     printf("Enter the first string:\n");
-    scanf("%c", string1);
+    scanf("%s", string1);
     printf("Enter the second string:\n");
-    scanf("%c", string2);
+    scanf("%s", string2);
 }
 
 int stringcompare(char *string1, char *string2){
     int i=0;
-    int result
-    for(string1[i])
+    int result=0;
+    for(i=0; string1[i]==string2[i] && string1[i] != '\0' && string2[i] != '\0'; i++){
+        if(string1[i]>string2[i]){
+            result=1;
+        }
+        else{
+            if(string1[i]<string2[i]){
+                result=2;
+            }
+        }
+        return result;
+    }
 
 }
 
 void output(char *string1, char *string2, int result){
+    if(result==1){
+        printf("The greater string is: %s", string1);
+    }
+
+    else if(result==2){
+            printf("The greater string is: %s", string2);
+        }
+    
 
 }
 
 int main(void){
+    int result=0;
     char string1[100], string2[100];
     input_two_strings(&string1, &string2);
-    int result;
     result = stringcompare(&string1, &string2);
-
+    output(&string1, &string2, result);
 }
