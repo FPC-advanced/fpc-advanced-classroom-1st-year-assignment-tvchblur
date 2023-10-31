8438dc06ad832f5b4603e957899cf2101c504bad
tvchblur <141307481+tvchblur@users.noreply.github.com>
2023-10-31 11:58:06 +0530

    committed 10/31/2023, 11:58:05 AM

--- set01/problem10.c
+++ set01/problem10.c
@@ -42,7 +42,7 @@ void input_two_strings(char *string1, char *string2){
 int stringcompare(char *string1, char *string2){
     int i=0;
     int result=0;
-    for(i=0; string1[i]==string2[i] && string1[i] != '\0' && string2[i] != '\0'; i++){
+    for(i=0; string1[i]==string2[i] && string1[i] != '\0' && string2[i] != '\0'; i++);
         if(string1[i]>string2[i]){
             result=1;
         }
