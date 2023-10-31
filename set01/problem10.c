0de8b9a94fffc816b14e94f73c92b0e6b93665ce
tvchblur <141307481+tvchblur@users.noreply.github.com>
2023-10-31 11:07:46 +0530

    committed 10/31/2023, 11:07:44 AM

--- set01/problem10.c
+++ set01/problem10.c
@@ -24,4 +24,8 @@ void input_two_strings(char *string1, char *string2){
 
 }
 
-int stringcompare(char *string1, char *string2)
+int stringcompare(char *string1, char *string2){
+
+}
+
+void output(char *string1, char *string2, int result)
