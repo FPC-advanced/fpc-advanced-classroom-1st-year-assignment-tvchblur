0b8c7987db078dd31530cbed15dd99a7d5d65384
tvchblur <141307481+tvchblur@users.noreply.github.com>
2023-10-31 12:03:53 +0530

    committed 10/31/2023, 12:03:51 PM

---
+++ set01/problem11.c
@@ -0,0 +1,46 @@
+/*11. Write a C program to find the sum of 2 complex numbers
+
+***Function Declarations***
+```c
+struct _complex {
+	float real;
+	float imaginary;
+}
+typedef struct _complex Complex;
+
+Complex input_complex();
+Complex add_complex(Complex a, Complex b);
+void output(Complex a, Complex b, Complex sum);
+```
+
+***Input***
+```
+2 3
+4 5
+```
+
+***Output***
+```
+The sum of 2+3i and 4+5i is 6+8i*/
+
+#include <stdio.h>
+
+struct _complex{
+    float real;
+    float imaginary;
+};
+typedef struct _complex Complex;
+
+Complex input_complex(){
+
+}
+Complex add_complex(Complex a, Complex b){
+
+}
+void output(Complex a, Complex b, Complex sum){
+
+}
+
+int main(void){
+    
+}
