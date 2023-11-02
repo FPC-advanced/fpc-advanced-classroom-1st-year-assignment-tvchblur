fefac8bf9eddd505473694281ee66d37e8f400b5
tvchblur <141307481+tvchblur@users.noreply.github.com>
2023-11-02 16:06:27 +0530

    committed 11/2/2023, 4:06:26 PM

---
+++ set02/problem01.c
@@ -0,0 +1,38 @@
+/*# Set 2
+
+01.  Write a program to find the area of a triangle.
+
+***Function Declarations***
+```c
+void input(float *base, float *height);
+void find_area(float base , float height, float *area);
+void output(float base, float height, float area);
+```
+
+***Input***
+```
+1
+2
+```
+
+***Output***
+```
+The area of the traingle with base 1.000000 and height 2.000000 is 1.000000*/
+
+#include <stdio.h>
+
+void input(float *base, float *height){
+
+}
+
+void find_area(float base, float height, float *area){
+
+}
+
+void output(float base, float height, float area){
+
+}
+
+int main(void){
+    
+}
