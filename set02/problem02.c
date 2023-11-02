142dd0a4721834970fc71a7024da110459a11500
tvchblur <141307481+tvchblur@users.noreply.github.com>
2023-11-02 16:22:57 +0530

    committed 11/2/2023, 4:22:56 PM

--- set02/problem02.c
+++ set02/problem02.c
@@ -1,26 +1,15 @@
 /*Write a program to find if a triangle is scalene.
 
 > A triangle is scalene if all the three sides of the triangle are not equal to one another
-
-***Function Declarations***
-```c
-int input_side();
-int check_scalene(int a, int b, int c);
-void output(int a, int b, int c, int isscalene);
-```
-
 ***Input***
-```
 5
 4
 5
-```
-
 ***Output***
-```
 The triangle with sides 5, 4 and 5 is not scalene*/
 
 int input_side(){
+    
 
 }
 
@@ -33,5 +22,5 @@ void output(int a, int b, int c, int isscalene){
 }
 
 int main(void){
-    
+
 }
