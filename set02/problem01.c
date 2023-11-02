9cb8e76147fbbcc3b5b6479af98cfd875c789099
tvchblur <141307481+tvchblur@users.noreply.github.com>
2023-11-02 16:13:57 +0530

    committed 11/2/2023, 4:13:56 PM

--- set02/problem01.c
+++ set02/problem01.c
@@ -34,6 +34,7 @@ void output(float base, float height, float area){
 int main(void){
     float base, height, area;
     input(&base, &height);
-    find_area(base, height &area);
+    find_area(base, height, &area);
     output(base, height, area);
+    return 0;
 }
