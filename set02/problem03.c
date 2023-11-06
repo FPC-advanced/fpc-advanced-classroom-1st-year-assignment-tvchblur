cc8d530c32f09ca3ee19365c563e54501e9ab700
tvchblur <141307481+tvchblur@users.noreply.github.com>
2023-11-06 09:43:03 +0530

    committed 11/6/2023, 9:43:02 AM

--- set02/problem03.c
+++ set02/problem03.c
@@ -32,8 +32,6 @@ int is_composite(int n){
     int i=0;
     int flag=0;
         for(i=0; i<=sqrt(n); i++){
-            
-        }
             if(n%i==0){
                 flag = 0;
             }
@@ -42,6 +40,7 @@ int is_composite(int n){
                 flag=1;
             }
             return flag;
+        }
 }
 
 void output(int n, int result){
