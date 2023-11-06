5deca65585260824844591b3d6b06d73ceb41279
tvchblur <141307481+tvchblur@users.noreply.github.com>
2023-11-06 09:42:33 +0530

    committed 11/6/2023, 9:42:32 AM

--- set02/problem03.c
+++ set02/problem03.c
@@ -31,7 +31,9 @@ int input_number(){
 int is_composite(int n){
     int i=0;
     int flag=0;
-        for(i=0; i<=sqrt(n); i++);
+        for(i=0; i<=sqrt(n); i++){
+            
+        }
             if(n%i==0){
                 flag = 0;
             }
