71dc1cd633a5f1e5836932861d271d0a35a9b237
tvchblur <141307481+tvchblur@users.noreply.github.com>
2023-11-06 10:46:19 +0530

    committed 11/6/2023, 10:46:18 AM

--- set02/problem04.c
+++ set02/problem04.c
@@ -29,6 +29,7 @@ int input_array_size(){
 void input_array(int n, int a[n]){
     int index=0;<
     for(index=0;index<n;index++){
+        printf("Append a number to the array:")
         a[index]
     }
 }
