a04a2027636fa4b87e96ebf2bb35da3bda599a44
tvchblur <141307481+tvchblur@users.noreply.github.com>
2023-11-08 19:08:37 +0530

    committed 11/8/2023, 7:08:35 PM

--- set02/problem06.c
+++ set02/problem06.c
@@ -1,36 +1,31 @@
 /*Write a program to reverse a string.
-
 ***Function Declarations***
-```c
 void input_string(char *a);
 void str_reverse(char *str, char *rev_str);
 void output(char *a, char *reverse_a);
-```
-
 ***Input***
-```
 hello
-```
-
 ***Output***
-```
 olleh*/
 
-void input_string(char *a){
+void input_string(char *str){
     printf("Enter a string to be reversed:\n");
-    scanf("%s", a);
+    scanf("%s", str);
 }
 
 void str_reverse(char *str, char *rev_str){
-
+    int i=0;
+    for(i=0; str[i]!='\0';i++){
+        rev_str[i] = str[-i]; // if logical error, change condition in line 18 to for(i=0; str[-i]!='\0';i++)
+    }
 }
 
-void output(char *a, char *reverse_a){
+void output(char *str, char *rev_str){
 
 }
 
 int main(void){
-    char a[100];
-    input_string(a);
+    char str[100], rev_str[100];
+    input_string(str);
 
 }
