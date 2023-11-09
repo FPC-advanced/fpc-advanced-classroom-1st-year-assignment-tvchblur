c46b675b00625fcf99cfe688f054b27061146224
tvchblur <141307481+tvchblur@users.noreply.github.com>
2023-11-09 13:15:23 +0530

    committed 11/9/2023, 1:15:22 PM

--- set02/problem07.c
+++ set02/problem07.c
@@ -7,9 +7,5 @@ void find_area(Traingle *t);
 void output(Triangle t);
 i:2
 3
-```
-
-***Output***
-```
-The area of triangle wwith base = 2.000000 and altitude = 3.000000 is 3.000000*/
+o:The area of triangle wwith base = 2.000000 and altitude = 3.000000 is 3.000000*/
 
