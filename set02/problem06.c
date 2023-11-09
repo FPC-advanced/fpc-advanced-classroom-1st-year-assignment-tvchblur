806faf0b0523bf99dc2d77c1f26be2acbdea15da
tvchblur <141307481+tvchblur@users.noreply.github.com>
2023-11-09 11:18:10 +0530

    committed 11/9/2023, 11:18:08 AM

--- set02/problem06.c
+++ set02/problem06.c
@@ -15,7 +15,7 @@ void input_string(char *str){
 }
 
 void str_reverse(char *str, char *rev_str){
-    int i=0 ,rev_str;
+    int i=0;
     for(i=0; str[i]!='\0';i++){
         rev_str[i] = str[-i];
     }
