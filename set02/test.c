87b32787991db133ac62edec3755cc9f0f21835c
tvchblur <141307481+tvchblur@users.noreply.github.com>
2023-11-09 17:04:24 +0530

    committed 11/9/2023, 5:04:23 PM

--- set02/test.c
+++ set02/test.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(void){
-    char str[100] = "hi i'm trisha.";
+    char str[100] = "gjdnr;g ehoejfoiwe reofniewio reifwnofiw.";
     int i=0;
     int counts = 0;
     for(i=0; str[i]!='\0'; i++){
