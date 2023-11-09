79f9d609497d4fc6c57ad4b6e6511da7a9b3dc70
tvchblur <141307481+tvchblur@users.noreply.github.com>
2023-11-09 17:08:24 +0530

    committed 11/9/2023, 5:08:23 PM

--- set02/test.c
+++ set02/test.c
@@ -9,8 +9,7 @@ void input(char *str[100]){
 int main(void){
     char str[100];
     input(str);
-    int i=0;
-    int counts = 0;
+    int i=0, counts = 0;
     for(i=0; str[i]!='\0'; i++){
         if(str[i]==' '){
             counts++;
