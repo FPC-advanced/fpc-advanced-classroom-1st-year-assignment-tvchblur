f2348d76f1ef42d3f3a7b7834fbecfa275e0f034
tvchblur <141307481+tvchblur@users.noreply.github.com>
2023-11-09 13:14:54 +0530

    committed 11/9/2023, 1:14:52 PM

---
+++ set02/problem07.c
@@ -0,0 +1,15 @@
+/*typedef struct _triangle {
+	float base, altitude, area;
+} Triangle;
+
+Triangle input_triangle();
+void find_area(Traingle *t);
+void output(Triangle t);
+i:2
+3
+```
+
+***Output***
+```
+The area of triangle wwith base = 2.000000 and altitude = 3.000000 is 3.000000*/
+
