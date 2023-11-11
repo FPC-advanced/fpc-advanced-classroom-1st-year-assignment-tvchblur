d05a3f85bc078476119f8c2d567a594c26a82f6d
tvchblur <141307481+tvchblur@users.noreply.github.com>
2023-11-11 12:40:46 +0530

    committed 11/11/2023, 12:40:44 PM

--- set03/problem01.c
+++ set03/problem01.c
@@ -5,6 +5,8 @@ void output(float x1, float y1, float x2, float y2, float distance);
 I:1 1
 2 2
 O:The distance between point (1.0000000, 1.0000000) and (2.0000000, 2.0000000) is 1.4142*/
+#include <stdio.h>
+#include <math.h>
 
 void input(float *x1, float *y1, float *x2, float *y2){
     printf("Enter the x and y values of the first point:\n");
