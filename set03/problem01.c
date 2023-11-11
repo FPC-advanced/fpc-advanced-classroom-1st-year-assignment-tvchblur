38ac1e804a5eae69ac447733eb10469c53bf2b6d
tvchblur <141307481+tvchblur@users.noreply.github.com>
2023-11-11 12:43:16 +0530

    committed 11/11/2023, 12:43:14 PM

--- set03/problem01.c
+++ set03/problem01.c
@@ -16,7 +16,7 @@ void input(float *x1, float *y1, float *x2, float *y2){
 }
 
 float find_distance(float x1, float y1, float x2, float y2){
-    float distance = ((pow(x1, 2)-pow(x2,2)) + (y1**2-y2**2))**(1/2);
+    float distance = ((pow(x1, 2)-pow(x2,2)) + (pow(y1,2)-pow(y2,2)));
 }
 
 void output(float x1, float y1, float x2, float y2, float distance){
