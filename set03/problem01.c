c7ce6af4fa8752dd8be67244aa796b6c12e7886c
tvchblur <141307481+tvchblur@users.noreply.github.com>
2023-11-11 12:49:46 +0530

    committed 11/11/2023, 12:49:45 PM

--- set03/problem01.c
+++ set03/problem01.c
@@ -28,7 +28,6 @@ int main(void){
     float x1,x2,y1,y2,distance;
     input(&x1,&y1,&x2,&y2);
     distance = find_distance(x1,y1,x2,y2);
-    
-
-
+    output(x1,y1,x2,y2,distance);
+    return 0;
 }
