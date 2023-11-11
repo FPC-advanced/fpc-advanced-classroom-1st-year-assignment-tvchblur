bcee7347f376a19710581a9e3378d29645a8249d
tvchblur <141307481+tvchblur@users.noreply.github.com>
2023-11-11 12:33:16 +0530

    committed 11/11/2023, 12:33:14 PM

--- set03/problem01.c
+++ set03/problem01.c
@@ -7,7 +7,8 @@ I:1 1
 O:The distance between point (1.0000000, 1.0000000) and (2.0000000, 2.0000000) is 1.4142*/
 
 void input(float *x1, float *y1, float *x2, float *y2){
-    
+    printf("Enter the x and y values of the first point:\n");
+    scanf("%f%f", )
 
 }
 
