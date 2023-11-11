f9880ae57efc141ca1488a750db36f06d931ff84
tvchblur <141307481+tvchblur@users.noreply.github.com>
2023-11-11 12:21:46 +0530

    committed 11/11/2023, 12:21:44 PM

---
+++ set03/problem01.c
@@ -0,0 +1,8 @@
+/*Write a program to find the distance between 2 points
+void input(float *x1, float *y1, float *x2, float *y2);
+float find_distance(float x1, float y1, float x2, float y2);
+void output(float x1, float y1, float x2, float y2, float distance);
+I:1 1
+2 2
+O:The distance between point (1.0000000, 1.0000000) and (2.0000000, 2.0000000) is 1.4142*/
+
