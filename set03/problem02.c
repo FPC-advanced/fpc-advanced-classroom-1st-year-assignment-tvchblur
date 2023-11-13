9944996e6a6763a6632cc671a4369a7231a709fd
tvchblur <141307481+tvchblur@users.noreply.github.com>
2023-11-13 16:17:28 +0530

    committed 11/13/2023, 4:17:27 PM

---
+++ set03/problem02.c
@@ -0,0 +1,12 @@
+/*Write a program to find whether the given 3 points form a triangle
+void input_triangle(float *x1, float *y1, float *x2, float *y2, float *x3, float *y3);
+int is_triangle(float x1, float y1, float x2, float y2,float x3, float y3);
+void output(float x1, float y1, float x2, float y2,float x3, float y3, int result);
+I:1 1
+0 0
+1 1
+O:The points (1.0, 1.0), (0.0, 0.0) and (1.0, 1.0) do not form a triangle*/
+
+#include <stdio.h>
+
+void input_triangle(float *x1, float *y1, float)
