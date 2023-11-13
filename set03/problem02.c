60dbcd75b7813b5c049e515d1db81a3c2b95c8d1
tvchblur <141307481+tvchblur@users.noreply.github.com>
2023-11-13 16:27:58 +0530

    committed 11/13/2023, 4:27:57 PM

--- set03/problem02.c
+++ set03/problem02.c
@@ -15,10 +15,11 @@ void input_triangle(float *x1, float *y1, float *x2, float *y2, float *x3, float
     printf("Enter the x and y coordinates of the second point:\n");
     scanf("%f%f", x2,y2);
     printf("Enter the x and y coordinates of the third point:\n");
-    scanf("%f%f", x3,y3);
+    scanf("%f%f", x3,y3);    
+}
+int is_triangle(float x1, float y1, float x2, float y2, float x3, float y3){
     
 }
-int is_triangle(float x1, float y1, float x2, float y2, float x3, float y3){}
 void output(float x1, float y1, float x2, float y2, float x3, float y3, int result){}
 
 int main(void){
