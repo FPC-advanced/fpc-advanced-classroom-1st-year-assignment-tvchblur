8fd1e4e09ff05c2050f93c8e00af26d4650b49c4
tvchblur <141307481+tvchblur@users.noreply.github.com>
2023-11-13 17:32:27 +0530

    committed 11/13/2023, 5:32:26 PM

--- set03/problem02.c
+++ set03/problem02.c
@@ -31,7 +31,8 @@ int is_triangle(float x1, float y1, float x2, float y2, float x3, float y3){
     If all three conditions are true, is_triangle= 1 (is triangle);
     */
    l1 = sqrt(pow(x1-x2,2)+pow(y1-y2,2));
-   l2 = sqrt(pow())
+   l2 = sqrt(pow(x2-x3,2)+pow(y1-y3,2));
+   l2 = sqrt(pow)
 
 }
 void output(float x1, float y1, float x2, float y2, float x3, float y3, int result){
