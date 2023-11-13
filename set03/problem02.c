49bb9b44672b7dddf08d9359a697219978ddb1b0
tvchblur <141307481+tvchblur@users.noreply.github.com>
2023-11-13 16:19:58 +0530

    committed 11/13/2023, 4:19:57 PM

--- set03/problem02.c
+++ set03/problem02.c
@@ -10,5 +10,5 @@ O:The points (1.0, 1.0), (0.0, 0.0) and (1.0, 1.0) do not form a triangle*/
 #include <stdio.h>
 
 void input_triangle(float *x1, float *y1, float *x2, float *y2, float *x3, float *y3){}
-int is_triangle(float *x1, float *y1, float *x2, float *y2, float *x3, float *y3){}
+int is_triangle(float x1, float y1, float x2, float y2, float x3, float y3){}
 
