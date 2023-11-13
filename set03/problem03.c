40be20530a135eeff98764c9d89725d8dde14331
tvchblur <141307481+tvchblur@users.noreply.github.com>
2023-11-13 19:19:44 +0530

    committed 11/13/2023, 7:19:42 PM

---
+++ set03/problem03.c
@@ -0,0 +1,14 @@
+/*Write a program to check if the given number is prime
+int input_number();
+int is_prime(int n);
+void output(int n, int result);
+```
+
+***Input***
+```
+3
+```
+
+***Output***
+```
+3 is a prime number*/
