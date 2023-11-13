9fccfde08c748b9e7dcdd40ca031caff7cf91515
tvchblur <141307481+tvchblur@users.noreply.github.com>
2023-11-13 19:42:31 +0530

    committed 11/13/2023, 7:42:30 PM

--- set03/problem03.c
+++ set03/problem03.c
@@ -9,7 +9,10 @@ O:3 is a prime number*/
 #include <math.h>
 
 int input_number(){
-    printf("Enter a number")
+    printf("Enter a number:\n");
+    int n;
+    scanf("%i", &n);
+    return n;
 }
 
 int is_prime(int n){
@@ -21,4 +24,5 @@ void output(int n, int result){
 
 int main(void){
     return 0;
+    int n=input_number();
 }
