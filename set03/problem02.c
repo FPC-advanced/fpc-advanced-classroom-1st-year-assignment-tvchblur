cb567ee9f4a2fa07c557f7bad2a61ff07c2c4a5e
tvchblur <141307481+tvchblur@users.noreply.github.com>
2023-11-13 16:18:58 +0530

    committed 11/13/2023, 4:18:57 PM

--- set03/problem02.c
+++ set03/problem02.c
@@ -9,4 +9,4 @@ O:The points (1.0, 1.0), (0.0, 0.0) and (1.0, 1.0) do not form a triangle*/
 
 #include <stdio.h>
 
-void input_triangle(float *x1, float *y1, float)
+void input_triangle(float *x1, float *y1, float *x2, float *y2, float )
