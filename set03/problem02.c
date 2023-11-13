cdb248616f098430adc3d8dc964955472a87c239
tvchblur <141307481+tvchblur@users.noreply.github.com>
2023-11-13 16:23:28 +0530

    committed 11/13/2023, 4:23:27 PM

--- set03/problem02.c
+++ set03/problem02.c
@@ -14,7 +14,7 @@ int is_triangle(float x1, float y1, float x2, float y2, float x3, float y3){}
 void output(float x1, float y1, float x2, float y2, float x3, float y3, int result){}
 
 int main(void){
-    
+    float x1, y1, x2, y2, x3, y3;
 }
 
 
