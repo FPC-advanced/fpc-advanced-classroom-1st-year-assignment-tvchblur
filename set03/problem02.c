4fa4e739105cb5d1eb906ef87799732663c5a386
tvchblur <141307481+tvchblur@users.noreply.github.com>
2023-11-13 16:24:28 +0530

    committed 11/13/2023, 4:24:27 PM

--- set03/problem02.c
+++ set03/problem02.c
@@ -15,7 +15,7 @@ void output(float x1, float y1, float x2, float y2, float x3, float y3, int resu
 
 int main(void){
     float x1, y1, x2, y2, x3, y3;
-    input_triangle(float *x1, float *y1, float *x2, float *y2, float *x3, float *y3);
+    input_triangle(&x1, &y1, &x2, &y2, &x3, y3);
     
 }
 
