34ddaae43b407cab562eeb12108532c04430a82d
tvchblur <141307481+tvchblur@users.noreply.github.com>
2023-11-13 17:43:57 +0530

    committed 11/13/2023, 5:43:56 PM

--- set03/problem02.c
+++ set03/problem02.c
@@ -40,7 +40,11 @@ int is_triangle(float x1, float y1, float x2, float y2, float x3, float y3){
     return flag;
 }
 void output(float x1, float y1, float x2, float y2, float x3, float y3, int result){
-    printf("The points (%f,%f), (%f,%f) and (%f%f) form a triangle.\n", x1, y1, x2, y2, x3, y3);
+    if(result==1){
+        printf("The points (%f,%f), (%f,%f) and (%f%f) form a triangle.\n", x1, y1, x2, y2, x3, y3);
+
+    }
+    
     printf("The points (%f,%f), (%f,%f) and (%f%f) do not form a triangle.\n", x1, y1, x2, y2, x3, y3);
 }
 
