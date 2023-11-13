f435a05e3e7433338afe2a71b367d1196336f167
tvchblur <141307481+tvchblur@users.noreply.github.com>
2023-11-13 17:45:57 +0530

    committed 11/13/2023, 5:45:56 PM

--- set03/problem02.c
+++ set03/problem02.c
@@ -45,7 +45,7 @@ void output(float x1, float y1, float x2, float y2, float x3, float y3, int resu
 
     }
     else{
-        printf("The points (%.1f,%f), (%.1f,%f) and (%f%f) do not form a triangle.\n", x1, y1, x2, y2, x3, y3);
+        printf("The points (%.1f,%.1f), (%.1f,%.1f) and (%.1f,%.1f) do not form a triangle.\n", x1, y1, x2, y2, x3, y3);
     }
 }
 
