9b4658cfafbc980a9e195a8e9cdb2293d899798f
tvchblur <141307481+tvchblur@users.noreply.github.com>
2023-11-15 11:56:13 +0530

    committed 11/15/2023, 11:56:12 AM

--- set03/problem04.c
+++ set03/problem04.c
@@ -1,15 +1,8 @@
 /*Write a program to find nth number in fibonacci sequence.
 
 > Fibonacci Sequence is of the form: `0, 1, 1, 2, 3, 5, 8, 13, ...`
-
-***Function Declarations***
-```c
-int input();
 int find_fibo(int n);
 void output(int n, int fibo);
-```
-
-***Input***
-```
-5
+I:5
 O:fibo(5) = 5*/
+
