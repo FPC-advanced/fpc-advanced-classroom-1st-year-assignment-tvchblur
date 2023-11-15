57c27dad2ca6fff3d33d654e719adb6210047f30
tvchblur <141307481+tvchblur@users.noreply.github.com>
2023-11-15 19:04:55 +0530

    committed 11/15/2023, 7:04:55 PM

--- set01/problem09.c
+++ set01/problem09.c
@@ -1,46 +1,37 @@
 /*
 9. Write a C program to find the [square root](https://en.wikipedia.org/wiki/Methods_of_computing_square_roots#Babylonian_method) of a number.
-
-***Function Declarations***
-```c
 float input();
 float square_root(float n);
 void output(float n, float sqrroot);
-```
-
-***Input***
-```
-Enter number: 49
-```
-
-***Output***
-```
-Square root of 49.0 is 7.0
+i:Enter number: 49
+o:Square root of 49.0 is 7.0
 */
 
 #include <stdio.h>
 
 float input(){
     printf("Enter a number:\n");
-    int n;
-    scanf("%f, &n");
+    float n;
+    scanf("%f", &n);
     return n;
 }
 
 float square_root(float n){
-    int x;
-    int y;
-    
-    
-
+    float i;
+    for(i=1.4142;i*i<=n;i+=0.0001){
+        continue;
+    }
+    return i;
 }
 
-void output(float m, float sqrroot){
-
+void output(float n, float sqrroot){
+    printf("The square root of %.2f is %.3f", n, sqrroot);
 }
 
 int main(void){
     float n;
     n = input();
-
+    float sqrroot = square_root(n);
+    output(n, sqrroot);
+    return 0;
 }
