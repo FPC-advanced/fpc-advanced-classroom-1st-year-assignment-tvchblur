fff55a5c9dbf0b52a76ff926e8ace340e5e06914
tvchblur <141307481+tvchblur@users.noreply.github.com>
2023-11-20 09:33:35 +0530

    committed 11/20/2023, 9:33:34 AM

--- set03/problem06.c
+++ set03/problem06.c
@@ -1,22 +1,13 @@
 /*6. Write a program to find the index of a substring of a string
-
-***Function Declarations***
-```c
 void input_string(char* a, char* b);
 int sub_str_index(char* string, char* substring);
 void output(char *string, char *substring, int index);
-```
-
-***Input***
-```
-helloworldhello
+I:helloworldhello
 world
-```
-
-***Output***
-```
-The index of 'world' in 'helloworldhello' is 5*/
+O:The index of 'world' in 'helloworldhello' is 5*/
 
 void input_string(char *a, char *b){}
 int sub_str_index(char *string, char *substring){}
 void output(char *string, char *substring, int index){}
+
+int main(voi)
